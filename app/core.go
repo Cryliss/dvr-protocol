3bb4a8f6116b06b345523d9c07aecb57695fdc82
Cryliss <sabrabilodeau@gmail.com>
2021-11-28 17:17:20 -0800

    Create individual functions for each command

--- app/core.go
+++ app/core.go
@@ -13,7 +13,7 @@ import (
 // New returns a new app
 func New(s types.Server) *Application {
 	a := Application{
-		commands: commands,
+		Commands: commands,
 		server:   s,
 	}
 
@@ -60,183 +60,184 @@ func (a *Application) parseInput(userInput string) error {
 	switch inputArgs[0] {
 	case "1":
 		fallthrough
-	case a.commands["1"]:
-		// If we have more than 2 input args, that means they want to see
-		// the command information for multiple commands, so let's loop
-		// through each of them
-		if numArgs > 2 {
-			for i := 1; i < numArgs; i++ {
-				a.help(inputArgs[i])
-			}
-			return nil
-		}
-
-		// If we only have 2, then the user just wants to see this one command
-		if numArgs == 2 {
-			a.help(inputArgs[1])
-			return nil
-		}
-
-		// We only had help in our input, so they want the full list
-		a.help("")
-		return nil
+	case a.Commands["1"]:
+		return a.help(inputArgs)
 	case "2":
 		fallthrough
-	case a.commands["2"]:
+	case a.Commands["2"]:
 		// Do we have the proper number of arguments?
 		if numArgs < 4 {
-			return UPDERR
+			return ErrUpd
 		}
-
-		// Let's get the first ID# given
-		id1, err := strconv.ParseInt(inputArgs[1], 10, 32)
-		if err != nil {
-			e := fmt.Sprintf("%s ERROR: error parsing input id1: %v\n", a.commands["2"], err)
-			return errors.New(e)
-		}
-
-		// Let's get the second ID# given
-		id2, err := strconv.ParseInt(inputArgs[2], 10, 32)
-		if err != nil {
-			e := fmt.Sprintf("%s ERROR: error parsing input id2: %v\n", a.commands["2"], err)
-			return errors.New(e)
-		}
-
-		// Let's check if the link cost given was "inf"
-		if inputArgs[3] == "inf" {
-			inputArgs[3] = "-1"
-		}
-
-		// Parse the link cost into an int
-		cost, err := strconv.Atoi(inputArgs[3])
-		if err != nil {
-			e := fmt.Sprintf("%s ERROR: error parsing input cost: %v\n", a.commands["2"], err)
-			return errors.New(e)
-		}
-
-		// Perform the update
-		if err := a.server.Update(uint16(id1), uint16(id2), cost); err != nil {
-			e := fmt.Sprintf("%s ERROR: %v\n", a.commands["2"], err)
-			return errors.New(e)
-		}
-		a.Out("%s SUCCESS\n", a.commands["2"])
-		return nil
+		return a.update(inputArgs)
 	case "3":
 		fallthrough
-	case a.commands["3"]:
-		// Call the servers step function and check for any errors
-		if err := a.server.Step(); err != nil {
-			e := fmt.Sprintf("%s ERROR: %v\n", a.commands["3"], err)
-			return errors.New(e)
-		}
-		a.Out("%s SUCCESS\n", a.commands["3"])
-		return nil
+	case a.Commands["3"]:
+		return a.step()
 	case "4":
 		fallthrough
-	case a.commands["4"]:
-		// Call the servers packets function and check for any errors
-		if err := a.server.Packets(); err != nil {
-			e := fmt.Sprintf("%s ERROR: %v\n", a.commands["4"], err)
-			return errors.New(e)
-		}
-		a.Out("%s SUCCESS\n", a.commands["4"])
-		return nil
+	case a.Commands["4"]:
+		return a.packets()
 	case "5":
 		fallthrough
-	case a.commands["5"]:
-		// Call the servers routing table function and check for any errors
-		if err := a.server.Display(); err != nil {
-			e := fmt.Sprintf("%s ERROR: %v\n", a.commands["5"], err)
-			return errors.New(e)
-		}
-		a.Out("%s SUCCESS\n", a.commands["5"])
-		return nil
+	case a.Commands["5"]:
+		return a.display()
 	case "6":
 		fallthrough
-	case a.commands["6"]:
+	case a.Commands["6"]:
 		// Do we have the proper number of arguments?
 		if numArgs != 2 {
-			return DISERR
+			return ErrDis
 		}
-
-		// Yes, so let's get the connection that we need to terminate
-		// and attempt to disable it
-		id, _ := strconv.ParseInt(inputArgs[1], 10, 64)
-
-		// Call the servers disable function and check for any errors
-		if err := a.server.Disable(uint16(id)); err != nil {
-			e := fmt.Sprintf("%s ERROR: %v\n", a.commands["6"], err)
-			return errors.New(e)
-		}
-		a.Out("%s SUCCESS\n", a.commands["6"])
-		return nil
+		return a.disable(inputArgs[1])
 	case "7":
 		fallthrough
-	case a.commands["7"]:
-		// Call the servers crash function and print the success message
-		a.server.Crash()
-		a.Out("%s SUCCESS\n", a.commands["7"])
-		return nil
+	case a.Commands["7"]:
+		return a.crash()
 	default:
 		// We didn't find a matching command for their input, let's throw an error
-		return INPERR
+		return ErrInp
 	}
+}
+
+// help prints the requested help commands
+func (a *Application) help(inputArgs []string) error {
+	numArgs := len(inputArgs)
+
+	// If we have more than 2 input args, that means they want to see
+	// the command information for multiple commands, so let's loop
+	// through each of them
+	if numArgs > 2 {
+		for i := 1; i < numArgs; i++ {
+			a.printHelp(inputArgs[i])
+		}
+		return nil
+	}
+
+	// If we only have 2, then the user just wants to see this one command
+	if numArgs == 2 {
+		a.printHelp(inputArgs[1])
+		return nil
+	}
+
+	// We only had printHelp in our input, so they want the full list
+	a.printHelp("")
 	return nil
 }
 
-// help prints the application commands
-func (a *Application) help(command string) {
+// printHelp prints the application commands
+func (a *Application) printHelp(command string) {
 	a.Out("\nApplication Commands\n")
 	a.Out("--------------------\n")
 
-	switch command {
-	case "":
-		cmds := `1. help - Displays available application commands
-2. update <server-ID1> <server-ID2> <new-link-cost> - Updates the link cost between the two servers
-3. step - Triggers the server to send the routing update right away
-4. packets - Displays the number of DVR packets this server has received since the last time this command was used
-5. display - Displays the current routing table, with the servers sorted in ascending order
-6. disable <server-ID> - Disables the link between to a given server
-7. crash - "Closes" all connections, to simulate a server crash
-`
-		a.Out(cmds)
-		break
-	case "1":
-		fallthrough
-	case "help":
-		a.Out("1. help - Displays available application commands\n")
-		break
-	case "2":
-		fallthrough
-	case "update":
-		a.Out("2. update <server-ID1> <server-ID2> <new-link-cost> - Updates the link cost between the two servers\n")
-		break
-	case "3":
-		fallthrough
-	case "step":
-		a.Out("3. step - Triggers the server to send the routing update right away\n")
-		break
-	case "4":
-		fallthrough
-	case "packets":
-		a.Out("4. packets - Displays the number of DVR packets this server has received since the last time this command was used\n")
-		break
-	case "5":
-		fallthrough
-	case "display":
-		a.Out("5. display - Displays the current routing table, with the servers sorted in ascending order\n")
-		break
-	case "6":
-		fallthrough
-	case "disable":
-		a.Out("6. disable <server-ID> - Disables the link between to a given server\n")
-		break
-	case "7":
-		fallthrough
-	case "crash":
-		a.Out("7. crash - 'Closes' all connections, to simulate a server crash\n")
-		break
-	default:
+	if command == "" {
+		for _, cmd := range helpText {
+			a.Out(cmd)
+		}
 		return
 	}
+
+	if len(command) == 1 {
+		if cmd, ok := helpText[commands[command]]; ok {
+			a.Out(cmd)
+		}
+		return
+	}
+
+	if cmd, ok := helpText[command]; ok {
+		a.Out(cmd)
+	}
+}
+
+// update updates the link cost between two servers
+func (a *Application) update(inputArgs []string) error {
+	// Let's get the first ID# given
+	id1, err := strconv.ParseInt(inputArgs[1], 10, 32)
+	if err != nil {
+		e := fmt.Sprintf("%s ERROR: error parsing input id1: %v\n", a.Commands["2"], err)
+		return errors.New(e)
+	}
+
+	// Let's get the second ID# given
+	id2, err := strconv.ParseInt(inputArgs[2], 10, 32)
+	if err != nil {
+		e := fmt.Sprintf("%s ERROR: error parsing input id2: %v\n", a.Commands["2"], err)
+		return errors.New(e)
+	}
+
+	// Let's check if the link cost given was "inf"
+	if inputArgs[3] == "inf" {
+		inputArgs[3] = "-1"
+	}
+
+	// Parse the link cost into an int
+	cost, err := strconv.Atoi(inputArgs[3])
+	if err != nil {
+		e := fmt.Sprintf("%s ERROR: error parsing input cost: %v\n", a.Commands["2"], err)
+		return errors.New(e)
+	}
+
+	// Perform the update
+	if err := a.server.Update(uint16(id1), uint16(id2), cost); err != nil {
+		e := fmt.Sprintf("%s ERROR: %v\n", a.Commands["2"], err)
+		return errors.New(e)
+	}
+	a.Out("%s SUCCESS\n", a.Commands["2"])
+	return nil
+}
+
+// step triggers the server to send a routing update automatically
+func (a *Application) step() error {
+	// Call the servers step function and check for any errors
+	if err := a.server.Step(); err != nil {
+		e := fmt.Sprintf("%s ERROR: %v\n", a.Commands["3"], err)
+		return errors.New(e)
+	}
+	a.Out("%s SUCCESS\n", a.Commands["3"])
+	return nil
+}
+
+// packets calls the server to display the # of packets received
+func (a *Application) packets() error {
+	// Call the servers packets function and check for any errors
+	if err := a.server.Packets(); err != nil {
+		e := fmt.Sprintf("%s ERROR: %v\n", a.Commands["4"], err)
+		return errors.New(e)
+	}
+	a.Out("%s SUCCESS\n", a.Commands["4"])
+	return nil
+}
+
+// display calls the server to display the current routing table
+func (a *Application) display() error {
+	// Call the servers routing table function and check for any errors
+	if err := a.server.Display(); err != nil {
+		e := fmt.Sprintf("%s ERROR: %v\n", a.Commands["5"], err)
+		return errors.New(e)
+	}
+	a.Out("%s SUCCESS\n", a.Commands["5"])
+	return nil
+}
+
+// disable calls the server to disable a server
+func (a *Application) disable(idString string) error {
+	// Yes, so let's get the connection that we need to terminate
+	// and attempt to disable it
+	id, _ := strconv.ParseInt(idString, 10, 64)
+
+	// Call the servers disable function and check for any errors
+	if err := a.server.Disable(uint16(id)); err != nil {
+		e := fmt.Sprintf("%s ERROR: %v\n", a.Commands["6"], err)
+		return errors.New(e)
+	}
+	a.Out("%s SUCCESS\n", a.Commands["6"])
+	return nil
+}
+
+// crash calls the server to crash it
+func (a *Application) crash() error {
+	// Call the servers crash function and print the success message
+	a.server.Crash()
+	a.Out("%s SUCCESS\n", a.Commands["7"])
+	return nil
 }
